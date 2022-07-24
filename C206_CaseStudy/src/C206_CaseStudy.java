3e5e72eaef8289b1608a48547763bec024711e9a
21023028 <21023028@LAPTOP-GUCA8NDU>
2022-07-24 20:31:02 +0800

    Nigel repo

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,6 +1,6 @@
 
 public class C206_CaseStudy {
-
+//if you see this message it means that you have pulled from nigel case study repo.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
