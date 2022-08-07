6cd803b4dc8e19572591fb02a1fbf4cf637040ee
21008768 <21008768@LAPTOP-D4H26CR6>
2022-08-07 21:37:15 +0800

    h

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -39,7 +39,7 @@ public class C206_CaseStudyTest {
 		b2 = new Bid("2101", "spoon", "liu3921@gmail.com", "tim213@gmail.com", 50.00);
 		il = new Item("ruler", "long long", 0.66, "1 August 2022", "8 August 2022", 1.00);
 		il2 = new Item("rubber", "pull and push", 1.00, "3 August 2022", "24 August 2022", 2.00);
-
+		bidList = new ArrayList<Bid>();
 		cc1 = new Category("shoes");
 		cc2 = new Category("clothes");
 		cc3 = new Category("electronics");
@@ -50,6 +50,9 @@ public class C206_CaseStudyTest {
 		// reset data to become null
 		a1 = null;
 		accountList = null;
+		b1 = null;
+		b2 = null;
+		bidList = null;
 	}
 
 	@Test
@@ -257,4 +260,67 @@ public class C206_CaseStudyTest {
 				"Active");
 		assertEquals(testOutput, allAccount);
 	}
-}
+	
+	@Test
+	public void bidAdd() {
+		// Test that bid list is not null, so that a new bid can be added 
+		// (boundary)
+		assertNotNull(bidList);
+
+		// Test that given an empty list, after adding one bid (normal),
+		// • the size of the list is 1
+		// • The bid just added is as same as the first bid of the list
+		b2 = C206_CaseStudy.addBid(bidList, "2100", "ruler", "max210@gmail.com", "pete129@gmail.com", 30.00);
+		assertEquals(1, bidList.size());
+		assertSame(b2, bidList.get(0));
+
+		// Test that Add another bid. (normal)
+		// • The size of the list is 2
+		// • The bid just added is as same as the 2nd bid of the list
+
+		b2 = C206_CaseStudy.addBid(bidList, "2101", "spoon", "liu3921@gmail.com", "tim213@gmail.com", 50.00);
+
+		assertEquals(2, accountList.size());
+		assertSame(a2, accountList.get(1));
+	}
+		
+
+
+	@Test
+	public void bidDelete() {
+		// Bid list is initially not null
+		assertNotNull(bidList);
+
+		// Bid list has two bids, after removing 1 bid, test if size of list become 1
+		C206_CaseStudy.addBid(bidList, "2100", "ruler", "max210@gmail.com", "pete129@gmail.com", 30.00);	
+		C206_CaseStudy.addBid(bidList, "2101", "spoon", "liu3921@gmail.com", "tim213@gmail.com", 50.00);
+		assertEquals(2, bidList.size());
+		bidList.remove(0);
+		assertEquals(1,bidList.size());
+
+	
+		// continue from number 2 test, remove another item, then test if list is empty
+		bidList.remove(0);
+		assertEquals(0,bidList.size());
+
+	}
+
+	
+	@Test
+	public void Viewbids() {
+
+		// Account list is initially not null
+		assertNotNull(bidList);
+
+		// Test that list is empty
+		String allAccount = C206_CaseStudy.retrieveAccounts(accountList);
+		assertEquals(bidList, bidList.size());
+
+		// Given an empty list, after adding 2 accounts, test if the size of the list is
+		// 2
+		C206_CaseStudy.addBid(bidList, "2100", "ruler", "max210@gmail.com", "pete129@gmail.com", 30.00);	
+		C206_CaseStudy.addBid(bidList, "2101", "spoon", "liu3921@gmail.com", "tim213@gmail.com", 50.00);
+		assertEquals(2, bidList.size());
+
+	
+	}
